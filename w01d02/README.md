780a3e1b82b4c7696d2dc8e3581bd9d9f016574e
andydlindsay@gmail.com <andydlindsay@gmail.com>
2021-02-02 11:29:51 -0800

    lecture end

--- w01d02/README.md
+++ w01d02/README.md
@@ -1,15 +1,27 @@
 # W01D02 - The Dev Workflow
 
 ### To Do
-- [ ] Curriculum overview
-- [ ] Example problem workflow
-- [ ] Problem solving tips
+- [x] Curriculum overview
+- [x] Example problem workflow
+- [x] Problem solving tips
 - [ ] Review git workflow
 
+Week 1 && 2: FOCAL
+Week 3: Web Servers
+Week 4: Client Side/Browser
+Week 5: Data Persistence (aka Databases)
+Week 6: Group Project - Midterm
 
+Week 7 && 8: React
+Week 9 && 10: Rails Week
+Week 11 && 12: Final Project
 
+### Compass
+* Instructions will become more vague over time
+* To do list
 
-
+### Problem
+* Write a program that takes in an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers.
 
 
 
---
+++ w01d02/command-line.js
@@ -0,0 +1,53 @@
+// Write a program that takes in an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers.
+
+// console.log('before:', process.argv);
+const args = process.argv;
+
+// args.shift();
+// args.shift();
+
+// args.splice(0, 2);
+
+const sliced = args.slice(2);
+
+// console.log();
+// console.log(sliced);
+// console.log();
+// console.log('after:', process.argv);
+
+// const nums = [ '5', '10', '15', '4.5', '3.14', '-50' ];
+
+let total = 0;
+// looping through the sliced array using for..of
+// for..of gives us the value/element every time
+for (const num of sliced) {
+  // console.log('current element:', num);
+  const converted = Number(num); // 4.5
+
+  // if (Number.isInteger(converted) === true) {
+  if (Number.isInteger(converted) && converted > 0) {
+    total += converted;
+    // total = total + num;
+  } 
+}
+
+console.log('total:', total);
+
+
+// const result = process
+//   .argv
+//   .slice(2)
+//   .map(Number)
+//   .filter(Number.isInteger)
+//   .filter(e => e > 0)
+//   .reduce((a, c) => a + c, 0);
+  
+// console.log('result:', result);
+
+
+
+
+
+
+
+// 
